8c5e8b1ae90f048fb711a69bfbd321f0784e866f
AhmAlkalii <hmkalkali@gmail.com>
2023-12-21 14:12:00 +0100

    somethings added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthorizationService} from './authorization.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Rentalsite';
+
+  get isLoggedIn(){
+    return this.authorizationService.isLoggedIn;
+  }
+
+  constructor(private authorizationService : AuthorizationService) { }
+
+  onLogout(){
+    this.authorizationService.logout();
+  }
 }
